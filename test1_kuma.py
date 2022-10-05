881f830c155b7e759fc6341acb7c0e74b86fe762
wrq <wangruqin35@gmail.com>
2022-10-05 11:48:15 +0900

    graw line

--- test1_kuma.py
+++ test1_kuma.py
@@ -1,7 +1,11 @@
+# from tkinter import N
+# from cv2 import HISTCMP_BHATTACHARYYA
+# from turtle import distance
 import matplotlib.pyplot as plt
 # impoty matplotlib
 import matplotlib.image as mpimg
 import numpy as np
+import math
 
 img = mpimg.imread('/home/wang/kuma_activate_analysis/background.png')
 
@@ -30,11 +34,66 @@ for i in range(len(x)):
     elif x[i] <  380:
         y[i]= -0.0054*x[i]*x[i] + 3.904*x[i] - 582.93         
 # y = np.array(y_list)
+h = 400
+r = h/218
+n = 5
+n_y = 16
+# the right = -h/190 * x = y
+# thr left = h/190 * x - 2h = y
+the_left_x = 218*190/-h
+the_right_x  = (218+2*h)*190/h
+
+the_right   = [the_right_x,218]
+the_left    = [the_left_x,218 ]
+the_h_point = [190,-h]
+
+#for line
+the_right_line_x = [the_right[0],the_h_point[0]]
+the_right_line_y = [the_right[1],the_h_point[1]]
+the_left_line_x = [the_left[0],the_h_point[0]]
+the_left_line_y = [the_left[1],the_h_point[1]]
+
+#for middle
+step  = (the_right_x - the_left_x)/n
+the_middle_x_n = []
+the_middle_line_x_n = []
+the_middle_line_y_n = []
+for i in range(n-1):
+    the_middle_x_n.append(the_left_x + (i+1)*step)
+    the_middle_line_x_n.append([the_left_x + (i+1)*step,the_h_point[0]])
+    the_middle_line_y_n.append([218,the_h_point[1]])
+
+
+# for yoko
+the_distance = math.dist(the_h_point, the_right)
+step_y = the_distance/n_y
+cos = (h+218)/the_distance
+b = [] 
+a = []
+for y in range(n_y-1):
+    the_distance = the_distance -step_y
+    b.append(the_distance)
+    a.append(218-b[y]*cos)
+
+
 
 ax = plt.imshow(img)
-# ax.plot(x,y)
 
-plt.plot(x,y)
+# plt.plot(x,y)
+# right
+plt.plot(the_right_x,218,".")
+# left
+plt.plot(the_left_x,218,".")
+#  hight
+plt.plot(190,-h,".") 
+# draw line
+plt.plot(the_right_line_x , the_right_line_y, 'bo', linestyle="--")
+plt.plot(the_left_line_x , the_left_line_y, 'bo', linestyle="--")
+
+for d in range(n-1):
+    plt.plot(the_middle_line_x_n[d] , the_middle_line_y_n[d], 'bo', linestyle="--")
+for dd in range(n_y-1):
+    plt.plot([0,380] , [a[dd],a[dd]], 'bo', linestyle="--")
 
 plt.show()
 # print(img)
